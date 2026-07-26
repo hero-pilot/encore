e336dde475d0e93072f2849b62e2cb6ac0cb8b2a
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-07-26 04:41:59 -0400

    Core entities

--- app/core/models.py
+++ app/core/models.py
@@ -2,26 +2,24 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 
-
 class UserManager(BaseUserManager):
-    def create_user(self, username,email, password=None, **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required!")
         if not username:
             raise ValueError("Username is required!")
-        user = self.model(usename = username,email=self.normalize_email(email), **extra_fields)
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,username, email, password):
+    def create_superuser(self, username, email, password):
         """Create and return a super user"""
-        user = self.create_user(username=username,email=email, password=password)
+        user = self.create_user(username=username, email=email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
@@ -33,4 +31,57 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "username"
-    REQUIRED_FIELDS =["email"]
+    REQUIRED_FIELDS = ["email"]
+
+class TicketStatus(models.TextChoices):
+    AVAILABLE = "AVAILABLE"
+    RESERVED = "RESERVED"
+
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.TextField()
+    capacity = models.PositiveBigIntegerField()
+
+class Performer(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    venue = models.ForeignKey(Venue, related_name="events", on_delete=models.CASCADE)
+    performer = models.ForeignKey(Performer, related_name="events", on_delete=models.CASCADE)
+    description = models.TextField()
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField(null=True, blank=True)
+
+class Seat(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name="seats")
+    row = models.CharField(max_length=10)
+    number = models.CharField(max_length=10)
+    section = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["venue", "row", "number"], 
+                name="unique_seat_placement",                
+                violation_error_message="This seat already exists in the venue."
+            )
+        ]
+
+class Ticket(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="tickets")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="tickets", null=True, blank=True)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE, related_name="tickets")
+    status = models.CharField(max_length=20, choices=TicketStatus.choices, default=TicketStatus.AVAILABLE)
+    price = models.PositiveBigIntegerField()
+    purchased_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["event", "seat"], 
+                name="each_seat_one_unique_event",                
+                violation_error_message="This seat and event already exist."
+            )
+        ]
