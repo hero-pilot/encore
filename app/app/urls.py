76e3e5695b9d3b4ae884f1b498099648e53045d3
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-07-26 09:34:21 -0400

    Added url endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("api/redoc/" , schema_view.with_ui("redoc", cache_timeout=0 ), name="schema-redoc"),
     path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0) , name="schema-swagger-ui"),
     path("api/v1/auth/", include("user.urls")),
+    path("api/v1/" , include("events.urls")),
 ]
