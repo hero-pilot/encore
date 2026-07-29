d8c2422413d8b5351b79a88982669987e2c47452
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-07-29 05:47:35 -0400

    Added a db constraint to stop double-click requests that might make two payments for the same ticket

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -18,3 +18,11 @@ class Payment(models.Model):
     status = models.CharField(max_length=20, choices=PaymentStatus.choices, default=PaymentStatus.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["ticket"],
+                condition=models.Q(status="pending"),
+                name="unique_pending_payment_per_ticket",
+            )
+        ]
